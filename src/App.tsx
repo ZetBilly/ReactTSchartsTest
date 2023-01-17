b9efa72b59e6ac59e37924f7ae277f0e75a2ef49
Alexander Plysheuski <Alexander_Plysheuski@epam.com>
2023-01-17 13:15:02 +0100

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import useFetch from 'react-fetch-hook';
+import { Bar, BarChart, Legend, Tooltip, XAxis, YAxis } from 'recharts';
+import createTrigger from "react-use-trigger";
+import useTrigger from 'react-use-trigger/useTrigger';
+
 import './App.css';
 
+type ApiResponse = Array<{
+  date: string;
+  units: number;
+}>;
+
+const requestTrigger = createTrigger();
+
+const RenderBarChart = ({ data = [] }: { data: ApiResponse | undefined }) => (
+  <BarChart width={800} height={600} data={data}>
+    <XAxis dataKey="date" />
+    <YAxis />
+    <Tooltip />
+    <Legend />
+    <Bar dataKey="units" fill="#8884d8" />
+  </BarChart>
+);
+
 function App() {
+  const requestTriggerValue = useTrigger(requestTrigger);
+
+  const { isLoading, data } = useFetch<ApiResponse>('https://my.api.mockaroo.com/graph.json?key=158c1970', {
+    depends: [requestTriggerValue]
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <div className='canvas-container'>
+        { isLoading && (<p>Loading...</p>)}
+        { !isLoading && <RenderBarChart {...{ data }} />}
+      </div>
+      <button onClick={() => requestTrigger()}>Fetch new data</button>
     </div>
   );
 }
